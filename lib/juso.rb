5c9250eee040d140caa0d6c604271ca5eedcc43d
ykpythemind <yukibukiyou@gmail.com>
2021-11-23 22:40:34 +0900

    impl

--- lib/juso.rb
+++ lib/juso.rb
@@ -2,7 +2,47 @@
 
 require_relative "juso/version"
 
+require 'json'
+
 module Juso
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Serializable
+    def juso_json(_)
+      nil
+    end
+  end
+
+  # Juso Context is serializer context
+  # xxxxx
+  class Context
+    def initialize(options: nil)
+      @options = options
+    end
+  end
+
+  def self.generate(object, context: Context.new)
+    JSON.fast_generate(_generate(object, context))
+  end
+
+  # generate returns hash (as json)
+  def self._generate(object, context)
+    case object
+    when nil, Numeric, String
+      return object
+    when Array
+      return object.map { |o| _generate(o, context) }
+    when Hash
+      return object.each_with_object({}) do |(k, v), acc|
+        acc[k] = _generate(v, context)
+      end
+    when Serializable
+      # class比較するの遅い？
+      return _generate(object.juso_json(context), context)
+    else
+      # puts object
+      # binding.irb
+      raise Error.new("cannot serialize object: #{object}. you must include Juso::Serializable")
+    end
+  end
 end
--- test/juso_test.rb
+++ test/juso_test.rb
@@ -3,11 +3,55 @@
 require "test_helper"
 
 class JusoTest < Minitest::Test
+  class User
+    include Juso::Serializable
+
+    def initialize(id:, nickname:)
+      @id = id
+      @nickname = nickname
+    end
+
+    def juso_json(*)
+      {
+        id: @id,
+        nickname: @nickname,
+      }
+    end
+  end
+
+  class Team
+    include Juso::Serializable
+
+    def initialize(id:, name:, users:)
+      @id = id
+      @name = name
+      @users = users
+    end
+
+    def juso_json(*)
+      {
+        id: @id,
+        name: @name,
+        users: @users
+      }
+    end
+  end
+
+  def setup
+    @users = [User.new(id: 1, nickname: 'ykpythemind'), User.new(id: 2, nickname: 'hogefuga')]
+
+    @team = Team.new(id: 1, name: 'strong team', users: @users)
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::Juso::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_serialize_json
+    expected = <<-JSON
+{"id":1,"name":"strong team","users":[{"id":1,"nickname":"ykpythemind"},{"id":2,"nickname":"hogefuga"}]}
+    JSON
+
+    assert_equal expected.strip, Juso.generate(@team)
   end
 end
